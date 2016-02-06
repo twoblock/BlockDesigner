9b5492749d31895e50364b6d00821f3fa5dafb2f
Lucas Choi <lucas.choi@twoblocktech.com>
2016-02-06 10:26:52 +0900

    chore($TestPMview): remove unused import class

--- TestPMview/src/swt_dnd/PM_viewtest.java
+++ TestPMview/src/swt_dnd/PM_viewtest.java
@@ -18,8 +18,6 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Device;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
